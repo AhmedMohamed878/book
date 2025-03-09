53954bbaf0252bac729730b62c71e6c33bfe8a1c
AhmedMohamed878 <am3753828@gmail.com>
2025-03-09 16:45:52 +0200

    Add files via upload

---
+++ tast.py
@@ -0,0 +1,17 @@
+Librari=[dict(horror="dse"),dict(romace="fsfsf"),dict(action="jshh")]
+print(Librari)
+
+#i=exit
+
+
+for i in Librari:
+    if Librari.remove() == False:
+        print ("Book not exsist")
+        Librari.append("fstsf")
+        Librari.remove("des")
+        print(Librari)
+
+    
+#while i == input("Enter exit: "):
+ #   break
+    
